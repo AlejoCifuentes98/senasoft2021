6e216e019b3b2f77dc6be553d99f0c516bc9b351
Kevin Vidal <kefevimi@gmai.com>
2021-10-19 16:11:17 -0500

    Modelos de la app cartas

--- apps/cartas/models.py
+++ apps/cartas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.nombre)
+
+class Cartas(models.Model):
+    nombre = models.CharField(max_length=80)
+    tipo   = models.ForeignKey()
+    def __str__(self):
+        return str(self.nombre)
