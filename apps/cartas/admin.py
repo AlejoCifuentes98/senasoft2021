18619cd1cd64433b8ad33211203c864577a3dd4a
Kevin Vidal <kefevimi@gmai.com>
2021-10-19 17:23:21 -0500

    correcion de admin de django

--- apps/cartas/admin.py
+++ apps/cartas/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Cartas, Tipo
 # Register your models here.
-admin.site.register(Cartas, Tipo)
+admin.site.register(Cartas)
+admin.site.register(Tipo)
