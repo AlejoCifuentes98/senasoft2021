bc975bf4d041cb153b9b705778f77dcba4b2e89d
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-19 16:29:47 -0500

    Modelos y templates

--- apps/partida/urls.py
+++ apps/partida/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.partida.views import inicio_view, partida_crear_view, partida_ingresar_view
 urlpatterns = [
+    #urls de la aplicación "partida"
     path('', inicio_view, name='inicio'),
     path('partida/crear', partida_crear_view, name='partida_crear'),
     path('partida/ingresar', partida_ingresar_view, name='partida_ingresar'),
