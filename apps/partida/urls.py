6c8187f0d5b3e1f5dada6f04b146a488bc31521f
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-19 16:20:31 -0500

    Views y urls app partida

--- apps/partida/urls.py
+++ apps/partida/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from apps.partida.views import inicio_view, partida_crear_view, partida_ingresar_view
 urlpatterns = [
     path('', inicio_view, name='inicio'),
-    path('partida/crear', crear_partida_view, name='crear_partida'),
+    path('partida/crear', partida_crear_view, name='partida_crear'),
     path('partida/ingresar', partida_ingresar_view, name='partida_ingresar'),
 ]
--- apps/partida/views.py
+++ apps/partida/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio_view(request):
+    return render(request, 'partida/inicio.html',locals())
+def partida_crear_view(request):
+    return render(request, 'partida/partida_crear.html',locals())
+def partida_ingresar_view(request):
+    return render(request, 'partida/partida_ingresar.html',locals())
+
