144e3d9955544ebb5172259429736d602465c63a
Kevin Vidal <kefevimi@gmai.com>
2021-10-20 18:24:01 -0500

    .

--- apps/partida/forms.py
+++ apps/partida/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from apps.partida.models import Partida, Jugador
+from django.contrib.auth.models import User
+
 class jugador_form(forms.ModelForm):
     class Meta:
         model = Jugador
@@ -13,8 +15,30 @@ class crear_partida_form(forms.ModelForm):
        fields = '__all__'
        exclude=['codigo_ingreso']
 
+class register_form(forms.Form):
+    username   = forms.CharField(label='Apodo', widget=forms.TextInput)
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput(render_value=False))
+    password2 = forms.CharField(label='Confirmar contraseña', widget=forms.PasswordInput(render_value=False))
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            c = User.objects.get(username = username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('El correo ingresado, ya se encuentra registrado')
 
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 == password2:
+            return password2
+        else:
+            raise forms.ValidationError('Las contraseñas no coinciden, intente de nuevo')
+
+class login_form(forms.Form):
+    nombre   = forms.CharField(label='Nombre', widget=forms.TextInput)
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput(render_value=False))
 
 class ingresar_partida_form(forms.Form):
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
