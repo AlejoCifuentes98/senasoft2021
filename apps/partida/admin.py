8f7ee6ae9f3daa48c7ccd526f9a09f2538195003
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-20 15:37:27 -0500

    Registro de modelos

--- apps/partida/admin.py
+++ apps/partida/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from apps.partida.models import Partida, Jugador
+from apps.partida.models import Partida, Jugador, Registro, Turno
 
 admin.site.register(Partida),
 admin.site.register(Jugador),
+admin.site.register(Registro),
+admin.site.register(Turno),
