ff834312d5642fddde107bae93e237aecad66613
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-20 13:45:42 -0500

    Documentacion  codigo para crear partida

--- apps/partida/views.py
+++ apps/partida/views.py
@@ -8,25 +8,18 @@ def inicio_view(request):
     return render(request, 'partida/inicio.html',locals())
 
 def partida_crear_view(request):
-    r = (randint(0, 10000000000))
-    c= hex(r) [2:]
-    codigo = ("{:.5}".format(c))
     if request.method == 'POST':
-        form_j = jugador_form(request.POST)
-        
-        
+        form_j = jugador_form(request.POST) #formulario para crear jugador
         if form_j.is_valid() :
-            form_j.save()     
-            r = (randint(0, 10000000000))
-            c= hex(r) [2:]
-            codigo = ("{:.5}".format(c))
-            Partida.objects.create(codigo_ingreso=codigo)
-                 
-            print("Partida creada con exito")
-             
-             
+            form_j.save() #Se guarda el nombre ingreado en el formulario por parte del jugador    
+            r = (randint(0, 10000000000))#Tomar rangos de numeros desde el 0 hasta un valor en especifico
+            c= hex(r) [2:] #Se pasa los numeros a hexadecimal, ademas se quita el prefijo "0x" el cual viene con todos los numeros hexadecimales
+            codigo = ("{:.5}".format(c)) #formatear el hexadecimal para obtener solo 5 digitos
+            Partida.objects.create(codigo_ingreso=codigo) #se captura l codigo y se pasa como parametro a la tabla Partida
+
+            return redirect('/partida/')               
     else: 
-        form_j = jugador_form()
+        form_j = jugador_form() #Si es un metodo Get, se pintará el formulario para ingresar el nombre del jugador
         
         
         return render(request, 'partida/partida_crear.html',locals())        
