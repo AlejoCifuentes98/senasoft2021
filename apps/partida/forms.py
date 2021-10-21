b5ae13ea61420e796c512ae2694f9295d1f5b209
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-21 09:05:17 -0500

    .

--- apps/partida/forms.py
+++ apps/partida/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from apps.partida.models import Partida, Jugador
 class jugador_form(forms.ModelForm):
     class Meta:
@@ -20,3 +21,28 @@ class ingresar_partida_form(forms.Form):
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     codigo = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
         
+
+class registro_form(forms.Form):
+    username   = forms.CharField(label='Nombre de usuario', widget=forms.TextInput)
+    clave_1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput(render_value=False))
+    clave_2 = forms.CharField(label='Confirmar contraseña', widget=forms.PasswordInput(render_value=False))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            c = User.objects.get(username = username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('El usuario ingresado, ya se encuentra registrado')
+
+    def clean_clave_2(self):
+        clave_1 = self.cleaned_data['clave_1']
+        clave_2 = self.cleaned_data['clave_2']
+        if clave_1 == clave_2:
+            return clave_2
+        else:
+            raise forms.ValidationError('Las contraseñas no coinciden, intente de nuevo')
+
+class login_form(forms.Form):
+    username   = forms.CharField(label='Usuario', widget=forms.TextInput)
+    clave = forms.CharField(label='Contraseña', widget=forms.PasswordInput(render_value=False))
--- apps/partida/urls.py
+++ apps/partida/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from apps.partida.views import inicio_view, partida_crear_view, partida_ingresar_view, partida_view, preguntar_view, acusar_view
+from apps.partida.views import inicio_view, partida_crear_view, partida_ingresar_view, partida_view, preguntar_view, acusar_view, registro_view, login_view, logout_view, perfil_view
 urlpatterns = [
     #urls de la aplicación "partida"
     path('', inicio_view, name='inicio'),
+    path('registro/', registro_view, name='registro'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+
+    path('perfil/', perfil_view, name='perfil'),
     path('partida/crear', partida_crear_view, name='partida_crear'),
     path('partida/ingresar', partida_ingresar_view, name='partida_ingresar'),
     
