ab6cb4978d0a58fe6f4a8ca5a3038e192f5936d8
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-21 09:05:01 -0500

    Perfil usuario, login, logout, registro

--- apps/partida/views.py
+++ apps/partida/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from apps.partida.forms import crear_partida_form, ingresar_partida_form, jugador_form
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from apps.partida.forms import crear_partida_form, ingresar_partida_form, jugador_form, registro_form, login_form
 from apps.partida.models import Partida, Jugador, Registro, Turno, Tablero
 from apps.cartas.models import Cartas
 
@@ -9,32 +11,91 @@ from random import randint # Funcion para generar los hexadecimales
 def inicio_view(request):
     return render(request, 'partida/inicio.html',locals())
 
+
 def partida_crear_view(request):
-    if request.method == 'POST':
-        form_j = jugador_form(request.POST) #formulario para crear jugador
-        if form_j.is_valid() :
-            jugador = form_j.save() #Se guarda el nombre ingreado en el formulario por parte del jugador    
+    jugador = Jugador.objects.get(id=request.user.id)
+    if request.method == 'POST': 
+        form_p = crear_partida_form(request.POST)       
+        if form_p.is_valid():
+            
             r = (randint(0, 10000000000))#Tomar rangos de numeros desde el 0 hasta un valor en especifico
             c= hex(r) [2:] #Se pasa los numeros a hexadecimal, ademas se quita el prefijo "0x" el cual viene con todos los numeros hexadecimales
             codigo = ("{:.5}".format(c)) #formatear el hexadecimal para obtener solo 5 digitos
             
             #Obtiene el primer objecto que se obtiene de un orden random de cada tipo
-            modulo = Cartas.objects.filter(tipo__nombre = "mod").order_by('?').first()
-            error = Cartas.objects.filter(tipo__nombre = "err").order_by('?').first()
-            desarrollador = Cartas.objects.filter(tipo__nombre = "dev").order_by('?').first()
+            # modulo = Cartas.objects.filter(tipo__nombre = "mod").order_by('?').first()
+            # error = Cartas.objects.filter(tipo__nombre = "err").order_by('?').first()
+            # desarrollador = Cartas.objects.filter(tipo__nombre = "dev").order_by('?').first()
 
-            partida = Partida.objects.create(
-                codigo_ingreso=codigo,#se captura l codigo y se pasa como parametro a la tabla Partida  
-                carta_err=error.id, 
-                carta_mod=modulo.id,
-                carta_des=desarrollador.id,)  
-            Registro.objects.create(jugador=jugador, partida=partida, jugador_numero='jugador 1')
-            
+            partida = Partida()
+            partida.codigo_ingreso=codigo#se captura el codigo y se pasa como parametro a la tabla Partida  
+            partida.save()
+                # carta_err=error.id, 
+                # carta_mod=modulo.id,
+                # carta_des=desarrollador.id,)  
+            registro = Registro()
+            registro.jugador = jugador
+            registro.partida = partida
+            registro.jugador_numero = 'jugador 1'
+            registro.save 
+            return redirect('/partida/')
     else:
-        form_j = jugador_form() #Si es un metodo Get, se pintará el formulario para ingresar el nombre del jugador
+        form_u = registro_form() #Si es un metodo Get, se pintará el formulario para ingresar el nombre del jugador
+        form_j = jugador_form()
         return render(request, 'partida/partida_crear.html',locals())        
     return render(request, 'partida/partida_crear.html',locals())
 
+def registro_view(request):
+    #seccion para crear una cuenta
+    if request.method == 'POST':
+        form_r =  registro_form(request.POST) #formulario para crear registrar 
+        form_j = jugador_form(request.POST)
+        
+        #usuario en la tabla User de django y la tabla jugador
+        if form_r.is_valid() and form_j.is_valid(): #validar si el formulario es valido
+            username =  form_r.cleaned_data['username'] #username 
+            clave_1 = form_r.cleaned_data['clave_1'] #Ingresar clave 1
+            clave_2 = form_r.cleaned_data['clave_2'] #Ingresar clave 2
+            u = User.objects.create_user(username= username, password= clave_2, is_superuser=False, is_staff=True) #Se crea el usuario
+            j = Jugador()
+            j.usuario = u
+            j.save()
+            return redirect('/login/')
+    else: 
+        form_r = registro_form() 
+        form_j = jugador_form()     
+        return render(request, 'partida/registro.html', locals())   
+    return render(request, 'partida/registro.html', locals())
+
+def login_view(request):
+    #seccion para loguearse 
+    usu = "" #variable para almacenar el username
+    cla = "" #variable para almacenar la clave
+    if request.method == 'POST':
+        form_l = login_form(request.POST)
+        if form_l.is_valid():
+            #Validaciones para el usuario y la contraseña
+            usu = form_l.cleaned_data['username'] 
+            cla = form_l.cleaned_data['clave']
+            usuario =authenticate(username=usu, password=cla)
+            if usuario is not None and usuario.is_active:
+                login(request, usuario)
+                return redirect('/perfil/')
+            else:
+                msj = 'Sus credenciales son incorrectas, verifique e intente nuevamente.'
+    else: 
+        form_l = login_form()                 
+        return render(request, 'partida/login.html', locals())
+    return render(request, 'partida/login.html', locals())
+
+def logout_view(request):
+    logout(request)
+    return redirect ('/login/')
+
+def perfil_view(request):
+    jugador = User.objects.get(id=request.user.id)
+    partidas = Registro.objects.filter() 
+    return render(request, 'partida/perfil.html',locals())
 def partida_ingresar_view(request):
     if request.method == 'POST':
         form_i=ingresar_partida_form(request.POST)
@@ -51,7 +112,7 @@ def partida_ingresar_view(request):
 
 def partida_view(request):
     jugador = Jugador.objects.get(jugador=request.user.id)
-    registro = Registro.object.get(jugador = jugador)
+    # registro = Registro.object.get(jugador = jugador)
     return render(request, 'partida/partida.html')
 
 def preguntar_view(request):
